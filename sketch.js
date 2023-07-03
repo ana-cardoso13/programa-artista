37bddb71a853c459a58084207b2b85533a614783
ana-cardoso13 <138503971+ana-cardoso13@users.noreply.github.com>
2023-07-03 12:02:07 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup( ) {
+  createCanvas(600, 600);
+}background ('white');
+}
+function draw() {
+  fill('green');
+if (mouseIsPressed) 
+    rect(mouseX,mouseY,20,35)
+stroke ('blue') ;
+
+}
